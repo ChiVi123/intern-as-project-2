f83ef119d503d7344e3cb5de28dab313d579f991
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-10-30 21:08:39 +0700

    add redux persist

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,7 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from '~modules/user/slice';
 
-export const store = configureStore({ reducer: { user: userReducer } });
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+    whiteList: ['user'],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: { user: persistedReducer },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+export const persistor = persistStore(store);
 export const useAppDispatch = () => useDispatch<typeof store.dispatch>();
--- src/view/app.tsx
+++ src/view/app.tsx
@@ -1,17 +1,20 @@
 import { ConfigProvider } from 'antd';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { themeConfig } from '~config';
-import { store } from '~core';
+import { persistor, store } from '~core';
 import { browserRouter } from '~routers/browser-router';
 
 function App() {
     return (
         <Provider store={store}>
-            <ConfigProvider theme={themeConfig}>
-                <RouterProvider router={browserRouter} />
-            </ConfigProvider>
+            <PersistGate persistor={persistor}>
+                <ConfigProvider theme={themeConfig}>
+                    <RouterProvider router={browserRouter} />
+                </ConfigProvider>
+            </PersistGate>
         </Provider>
     );
 }
