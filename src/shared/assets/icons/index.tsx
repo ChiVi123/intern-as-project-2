317b9006f93ce6a910ed9586f047fbee45484b46
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-10-30 20:52:11 +0700

    add camera outlined icon

--- src/shared/assets/icons/index.tsx
+++ src/shared/assets/icons/index.tsx
@@ -1,3 +1,4 @@
+export { default as CameraOutlinedIcon } from './camera-outlined';
 export { default as FileChartOutlinedIcon } from './file-chart-outlined';
 export { default as GridLayoutOutlinedIcon } from './grid-layout-outlined';
 export { default as HexagonDotCenterIcon } from './hexagon-dot-center';
