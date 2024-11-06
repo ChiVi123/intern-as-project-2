e738577b57e36413427eeb550990947295acd38d
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-11-07 00:05:41 +0700

    feature(device page): load single data

--- src/view/monitor/detail/page.tsx
+++ src/view/monitor/detail/page.tsx
@@ -1,7 +1,11 @@
 import { Col, Row, Typography } from 'antd';
+import { useLoaderData } from 'react-router-dom';
 import { cssHeading, cssPaper } from '~css-emotion';
+import { IDeviceEntity } from '~modules/device';
 
 function MonitorPage() {
+    const loader = useLoaderData() as IDeviceEntity;
+
     return (
         <>
             <Typography.Title level={3}>Quản lý thiết bị</Typography.Title>
@@ -13,36 +17,35 @@ function MonitorPage() {
                 <Row gutter={[0, 16]}>
                     <Col span={12}>
                         <Typography.Text strong>Mã thiết bị: </Typography.Text>
-                        <Typography.Text>KIO_01</Typography.Text>
+                        <Typography.Text>{loader.id}</Typography.Text>
                     </Col>
                     <Col span={12}>
                         <Typography.Text strong>Loại thiết bị: </Typography.Text>
-                        <Typography.Text>Kiosk</Typography.Text>
+                        <Typography.Text>{typeof loader.type !== 'object' && loader.type}</Typography.Text>
                     </Col>
 
                     <Col span={12}>
                         <Typography.Text strong>Tên thiết bị: </Typography.Text>
-                        <Typography.Text>Kiosk</Typography.Text>
+                        <Typography.Text>{loader.name}</Typography.Text>
                     </Col>
                     <Col span={12}>
                         <Typography.Text strong>Tên đăng nhập: </Typography.Text>
-                        <Typography.Text>Linhkyo011</Typography.Text>
+                        <Typography.Text>{loader.usernameDevice}</Typography.Text>
                     </Col>
 
                     <Col span={12}>
                         <Typography.Text strong>Địa chỉ IP: </Typography.Text>
-                        <Typography.Text>128.172.308</Typography.Text>
+                        <Typography.Text>{loader.ipAddress}</Typography.Text>
                     </Col>
                     <Col span={12}>
                         <Typography.Text strong>Mật khẩu: </Typography.Text>
-                        <Typography.Text>CMS</Typography.Text>
+                        <Typography.Text>{loader.passwordDevice}</Typography.Text>
                     </Col>
 
                     <Col>
                         <Typography.Text strong>Dịch vụ sử dụng:</Typography.Text>
                         <Typography.Paragraph>
-                            Khám tim mạch, Khám sản - Phụ khoa, Khám răng hàm mặt, Khám tai mũi họng, Khám hô hấp, Khám
-                            tổng quát.
+                            {loader.services?.map((item) => item.name).join(', ')}
                         </Typography.Paragraph>
                     </Col>
                 </Row>
--- src/view/monitor/detail/router.tsx
+++ src/view/monitor/detail/router.tsx
@@ -1,9 +1,47 @@
+import { getDoc } from 'firebase/firestore';
 import { lazy } from 'react';
 import { RouteObject } from 'react-router-dom';
+import { ResponseErrorRepo } from '~core';
+import { getDeviceById, IDeviceEntity } from '~modules/device';
+import { IServiceEntity } from '~modules/service';
+
+const defaultDevice: IDeviceEntity = {
+    id: '',
+    name: '',
+    ipAddress: '',
+    passwordDevice: '',
+    usernameDevice: '',
+    type: { label: '', value: '' },
+    actionStatus: { label: '', value: '' },
+    connectStatus: { label: '', value: '' },
+    services: [],
+    serviceRefs: [],
+};
 
 const monitorDetailRouter: RouteObject = {
     path: ':id',
     Component: lazy(() => import('./page')),
+    loader: async ({ params: { id } }): Promise<IDeviceEntity> => {
+        const res = await getDeviceById(id || '');
+
+        if (res instanceof ResponseErrorRepo) {
+            return defaultDevice;
+        }
+        if (!res.data) {
+            return defaultDevice;
+        }
+        if (!res.data.exists()) {
+            return defaultDevice;
+        }
+        const data = res.data.data();
+        const services = await Promise.all(
+            data.serviceRefs.map(async (ref) => {
+                const service = await getDoc(ref);
+                return { ...service.data(), id: service.id } as IServiceEntity;
+            }),
+        );
+        return { ...data, id: res.data.id, services };
+    },
 };
 
 export default monitorDetailRouter;
